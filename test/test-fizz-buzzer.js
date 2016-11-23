a0c2aef05a777f2d0cd11789e973800a0f526bfb
Benjamin White <benjamin.e.white1@gmail.com>
2016-11-23 12:46:59 -0500

    add edge case test

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -7,13 +7,13 @@ describe('fizzBuzzer', function() {
   it('should return "fizz-buzz" for multiples of 15', function() {
     [15, 30, 45].forEach(function(input) {
       fizzBuzzer(input).should.equal('fizz-buzz');
-    })
+    });
   });
 
   it('should return "fizz" for multiples of 3', function() {
     [3, 6, 9, 12].forEach(function(input) {
       fizzBuzzer(input).should.equal('fizz');
-    })
+    });
   });
 
   it('should return "buzz" for multiples of 5', function() {
@@ -28,4 +28,12 @@ describe('fizzBuzzer', function() {
     });
   });
 
+  it('should produce error if input isn\'t number', function() {
+    const badInputs = [true, false, 'cat', function() {}, [1, 2, 3]]
+    badInputs.forEach(function(input) {
+      (function() {
+          fizzBuzzer(input)
+      }).should.throw(Error);
+    });
+  });
 });
