f8a0ed3bc48ea75f1a5cfb44c52f7163ca33734c
Chris Tsongas <chris.tsongas@gmail.com>
2018-01-03 18:34:26 +0000

    update should to expect, add missing semicolons

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -1,4 +1,4 @@
-const should = require('chai').should();
+const expect = require('chai').expect;
 
 const fizzBuzzer = require('../fizzBuzzer');
 
@@ -6,34 +6,34 @@ describe('fizzBuzzer', function() {
 
   it('should return "fizz-buzz" for multiples of 15', function() {
     [15, 30, 45].forEach(function(input) {
-      fizzBuzzer(input).should.equal('fizz-buzz');
+      expect(fizzBuzzer(input)).to.equal('fizz-buzz');
     });
   });
 
   it('should return "fizz" for multiples of 3', function() {
     [3, 6, 9, 12].forEach(function(input) {
-      fizzBuzzer(input).should.equal('fizz');
+      expect(fizzBuzzer(input)).to.equal('fizz');
     });
   });
 
   it('should return "buzz" for multiples of 5', function() {
     [5, 10, 20].forEach(function(input) {
-      fizzBuzzer(input).should.equal('buzz');
+      expect(fizzBuzzer(input)).to.equal('buzz');
     });
   });
 
   it('should return number if not mult of 3 or 5', function() {
     [1, 2, 4, 7].forEach(function(input) {
-      fizzBuzzer(input).should.equal(input);
+      expect(fizzBuzzer(input)).to.equal(input);
     });
   });
 
   it('should produce error if input isn\'t number', function() {
-    const badInputs = [true, false, 'cat', function() {}, [1, 2, 3]]
+    const badInputs = [true, false, 'cat', function() {}, [1, 2, 3]];
     badInputs.forEach(function(input) {
-      (function() {
-          fizzBuzzer(input)
-      }).should.throw(Error);
+      expect(function() {
+          fizzBuzzer(input);
+      }).to.throw(Error);
     });
   });
 });
